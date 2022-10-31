3f66d329a0e112185bac8bfe1e3856a3d17823b8
Or <orlevy11@gmail.com>
2022-10-31 16:14:21 +0200

    first commit

---
+++ Animal.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self, animal_type, name) -> None:
+        self.animal_type = animal_type
+        self.name = name
+        
+    
+    def feed():
+        pass
+    
+        
